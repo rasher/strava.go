4f9db1783a7f3fa24e17db51ea41468f63b3c948
Jonas Häggqvist <rasher@rasher.dk>
2019-10-30 21:29:42 +0100

    Script to update from strava API

---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+CODEGEN_URL=http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/2.4.9/swagger-codegen-cli-2.4.9.jar
+
+pushd "$(dirname "$0")"
+
+git ls-files |grep -v "$(basename "$0")" |xargs git rm -f
+
+wget $CODEGEN_URL -O swagger-codegen-cli.jar
+java -jar swagger-codegen-cli.jar generate --input-spec https://developers.strava.com/swagger/swagger.json --lang go --output .
+rm -f swagger-codegen-cli.jar
+
+git add .
+
+popd
