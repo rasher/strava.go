9e72bf706fd903c9f4d403e820b925d3c09a5c04
Jonas Häggqvist <rasher@rasher.dk>
2020-03-15 16:22:11 +0100

    Fix repo url and abort on errors

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-CODEGEN_URL=http://central.maven.org/maven2/io/swagger/swagger-codegen-cli/2.4.12/swagger-codegen-cli-2.4.12.jar
+set -e
+CODEGEN_URL=https://repo.maven.apache.org/maven2/io/swagger/swagger-codegen-cli/2.4.12/swagger-codegen-cli-2.4.12.jar
 
 pushd "$(dirname "$0")"
 
