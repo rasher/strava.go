cc3a495745c5b98ea43b8a255bbf5ee55089c9c2
Jonas Häggqvist <rasher@rasher.dk>
2020-03-15 17:06:15 +0100

    Use openapi-generator

--- update.sh
+++ update.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 set -e
-CODEGEN_URL=https://repo.maven.apache.org/maven2/io/swagger/swagger-codegen-cli/2.4.12/swagger-codegen-cli-2.4.12.jar
+CODEGEN_URL=https://repo.maven.apache.org/maven2/org/openapitools/openapi-generator-cli/4.2.3/openapi-generator-cli-4.2.3.jar
+JAR=openapi-generator-cli.jar
 
 pushd "$(dirname "$0")"
 
-git ls-files |grep -v "$(basename "$0")" |xargs git rm -f
+if [ $(git ls-files |grep -v "$(basename "$0")" |wc -l) -gt 0 ]; then
+    git ls-files |grep -v "$(basename "$0")" |xargs git rm --ignore-unmatch -f
+fi
 
-wget $CODEGEN_URL -O swagger-codegen-cli.jar
+wget $CODEGEN_URL -O $JAR
 
-java -jar swagger-codegen-cli.jar generate -DpackageName=strava -DhideGenerationTimestamp=false --input-spec https://developers.strava.com/swagger/swagger.json --lang go --output .
-rm -f swagger-codegen-cli.jar
+java -jar $JAR generate \
+    --input-spec https://developers.strava.com/swagger/swagger.json \
+    --generator-name go \
+    --additional-properties=packageName=strava \
+    --additional-properties=hideGenerationTimestamp=false \
+    --skip-validate-spec \
+    --output .
+rm -f $JAR
 
 git add .
 
